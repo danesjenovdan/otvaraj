337d0c608a4890bf9b7b93f25baeec2d781e4068
patricijab <patricija.brecko@gmail.com>
2022-06-27 11:53:53 +0200

    add icons to map

--- src/components/MapOtvaraj.vue
+++ src/components/MapOtvaraj.vue
@@ -4,6 +4,14 @@ import { ref, onMounted } from 'vue'
 import "leaflet/dist/leaflet.css"
 import leaflet from "leaflet"
 import imgUrl from '../assets/icons/pin.svg'
+import churchUrl from '../assets/icons/cerkev.svg'
+import checkpoint1Url from '../assets/icons/checkpoint-levo-zgoraj.svg'
+import checkpoint2Url from '../assets/icons/checkpoint-desno-zgoraj.svg'
+import checkpoint3Url from '../assets/icons/checkpoint-spodaj.svg'
+import fence1Url from '../assets/icons/ograja-zgoraj.svg'
+import fence2Url from '../assets/icons/ograja-spodaj.svg'
+import parliamentUrl from '../assets/icons/parlament.svg'
+import vladaUrl from '../assets/icons/vlada.svg'
 
 const showChurch = ref(false)
 
@@ -28,17 +36,57 @@ onMounted(() => {
     scrollWheelZoom: false
   }).setView([45.816272, 15.973636], 19);
 
+  // disable moving the map
   map.dragging.disable();
 
+  // add map data
   leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap',
     maxZoom: 20,
   }).addTo(map);
+  // ---
 
-  let mapIcon = leaflet.icon({
-    iconUrl: imgUrl,
+  // create icons
+  let churchIcon = leaflet.icon({
+    iconUrl: churchUrl,
+    iconSize: [220, 220]
+  })
+
+  let checkpoint1Icon = leaflet.icon({
+    iconUrl: checkpoint1Url,
     iconSize: [60, 60]
   })
+
+  let checkpoint2Icon = leaflet.icon({
+    iconUrl: checkpoint2Url,
+    iconSize: [60, 60]
+  })
+
+  let checkpoint3Icon = leaflet.icon({
+    iconUrl: checkpoint3Url,
+    iconSize: [60, 60]
+  })
+  
+  let fence1Icon = leaflet.icon({
+    iconUrl: fence1Url,
+    iconSize: [350, 100]
+  })
+
+  let fence2Icon = leaflet.icon({
+    iconUrl: fence2Url,
+    iconSize: [550, 200]
+  })
+
+  let parliamentIcon = leaflet.icon({
+    iconUrl: parliamentUrl,
+    iconSize: [190, 190]
+  })
+
+  let vladaIcon = leaflet.icon({
+    iconUrl: vladaUrl,
+    iconSize: [200, 150]
+  })
+  // ---
 
   // icon
 //   var icon = L.icon({
@@ -52,23 +100,40 @@ onMounted(() => {
 //     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 // });
 
-  // church
-  const crkva = leaflet.marker([45.816426862740336, 15.973645607777067], {icon: mapIcon}).addTo(map);
-  crkva.on('click', log);
+  // add icons to map
+  const church = leaflet.marker([45.8164, 15.973645607777067], {icon: churchIcon}).addTo(map);
+  church.on('click', log);
 
-  // parliament
-  const latlngs1 = [[45.816717, 15.972668],[45.81665448099697, 15.973320536207437],[45.81601521042152, 15.973196831357253],[45.81607619378649, 15.97250287731962]];
-  const vlada = leaflet.polygon(latlngs1, {color: 'red'}).addTo(map);
-  vlada.on('click', log);
+  const parliament = leaflet.marker([45.81632, 15.97438], {icon: parliamentIcon}).addTo(map);
+  church.on('click', log);
+
+  const vlada = leaflet.marker([45.81637, 15.97296], {icon: vladaIcon}).addTo(map);
+  church.on('click', log);
+
+  const fence1 = leaflet.marker([45.81668, 15.973659769227755], {icon: fence1Icon}).addTo(map);
+  church.on('click', log);
+
+  const fence2 = leaflet.marker([45.81598, 15.97322], {icon: fence2Icon}).addTo(map);
+  church.on('click', log);
+
+  const checkpoint1 = leaflet.marker([45.81671, 15.97335], {icon: checkpoint1Icon, zIndexOffset: -10}).addTo(map);
+  church.on('click', log);
+
+  const checkpoint2 = leaflet.marker([45.816675, 15.97398], {icon: checkpoint2Icon, zIndexOffset: -10}).addTo(map);
+  church.on('click', log);
+
+  const checkpoint3 = leaflet.marker([45.81597, 15.97353], {icon: checkpoint3Icon, zIndexOffset: -10}).addTo(map);
+  church.on('click', log);
+
+  // vlada
+  const latlngs1 = [[45.816717, 15.972668], [45.81666, 15.97333], [45.81602, 15.97323], [45.81607, 15.97252]];
+  const vladaPoly = leaflet.polygon(latlngs1, {color: 'red'}).addTo(map);
+  vladaPoly.on('click', log);
 
   // sabor
-  const latlngs2 = [[45.81666289240078, 15.97411104033098],[45.81661032109227, 15.974738616156317],[45.81596053562144, 15.974642066028593],[45.815987873027055, 15.973970740926973]];
+  const latlngs2 = [[45.81663, 15.97405], [45.81662, 15.97481], [45.81585, 15.97475], [45.81594, 15.97393]];
   leaflet.polygon(latlngs2, {color: 'blue'}).addTo(map);
 
-  // policija
-  leaflet.circle([45.8167049494219, 15.973335622144996], {color: 'black', radius: 5}).addTo(map);
-  leaflet.circle([45.81665027529974, 15.974076342636806], {color: 'black', radius: 5}).addTo(map);
-  leaflet.circle([45.815977724108635, 15.973511212700387], {color: 'black', radius: 5}).addTo(map);
 
 });
 
